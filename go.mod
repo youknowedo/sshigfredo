c8185ae4d1c6eece188290927da07bfb2f1dc1cc
Filip Mårtensson <fenwikk@protonmail.com>
2024-08-11 09:40:45 +0200

    email sending & lost colors along the way

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
 	github.com/fatih/color v1.17.0 // indirect
 	github.com/gliderlabs/ssh v0.3.7 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	golang.org/x/crypto v0.26.0 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
+	"net/smtp"
+	"os"
 	"regexp"
 
 	"github.com/fatih/color"
 	"github.com/gliderlabs/ssh"
+	"github.com/joho/godotenv"
 	"golang.org/x/crypto/ssh/terminal"
 )
 
@@ -14,11 +18,21 @@ var (
 	helpCmd = regexp.MustCompile(`^help$`)
 	contact = regexp.MustCompile(`^contact$`)
 	exitCmd = regexp.MustCompile(`^exit$`)
+
+	validEmail = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	auth := smtp.PlainAuth("", os.Getenv("SMTP_EMAIL"), os.Getenv("SMTP_PASSWORD"), os.Getenv("SMTP_HOST"))
+
 	ssh.Handle(func(s ssh.Session) {
 		color.New(color.FgYellow).Fprint(s, "\nSigfredo\n\n")
+		io.WriteString(s, "type help to get started\n")
 
 		// TODO: Make something fun here!
 
@@ -35,25 +49,42 @@ func main() {
 					s.Exit(0)
 				case helpCmd.MatchString(line):
 					color.New(color.FgYellow).Fprint(s, "\nCommands:\n\n")
-					io.WriteString(s, "  help\n")
-					io.WriteString(s, "  contact\n")
-					io.WriteString(s, "  exit\n\n")
+					io.WriteString(s, "  help - Shows this message\n")
+					io.WriteString(s, "  contact - Hits me up. On mail but hits me up nonetheless.\n")
+					io.WriteString(s, "  exit - Exits this session\n\n")
 				case contact.MatchString(line):
-					term.SetPrompt("yru")
-					io.WriteString(s, "  Email:")
-					email, err := term.ReadLine()
-					if err != nil {
-						break
+					term.SetPrompt(": ")
+
+					// Aske for email until it's valid
+					var email string
+					for {
+						io.WriteString(s, "   Email")
+						email, err = term.ReadLine()
+						if err != nil {
+							break
+						}
+
+						if validEmail.MatchString(email) {
+							break
+						}
+
+						color.New(color.FgRed).Fprint(s, "Invalid email\n\n")
 					}
-					io.WriteString(s, "  Message:")
+					io.WriteString(s, "   Message")
 					message, err := term.ReadLine()
 					if err != nil {
 						break
 					}
 
-					color.New(color.FgYellow).Fprint(s, "\n\nThanks for contacting me!\n\n")
-					io.WriteString(s, "  Email: "+email+"\n")
-					io.WriteString(s, "  Message: "+message+"\n\n")
+					err = smtp.SendMail(os.Getenv("SMTP_HOST")+":"+os.Getenv("SMTP_PORT"), auth, os.Getenv("SMTP_EMAIL"), []string{"hello@sigfredo.fun"}, []byte("Subject: SSH from "+email+"\nTo: hello@sigfredo.fun\n\n"+message))
+					if err != nil {
+						color.New(color.FgRed).Fprint(s, "\nError sending message\n\n")
+						fmt.Println(err)
+					} else {
+						color.New(color.FgGreen).Fprint(s, "\nMessage sent\n\n")
+					}
+
+					term.SetPrompt("=> ")
 				}
 			}
 		}
